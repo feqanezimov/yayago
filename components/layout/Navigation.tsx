94f1cf5f71e1e8266e3241e1430f31469b730b08
feqanezimov <feqanezimov1@gmail.com>
2025-09-24 21:22:43 +0400

    Updated Navigation.tsx

--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -26,7 +26,7 @@ export function Navigation() {
               onClick={() => setIsOpen(true)}
               className="text-gray-900" // lg:hidden sinifi silindi ki, həmişə görünsün
             >
-              <Menu className="h-6 w-6" />
+              <Menu className="h-12 w-12" />
             </Button>
 
             {/* Logo - hamburger menyusunun yanında */}
